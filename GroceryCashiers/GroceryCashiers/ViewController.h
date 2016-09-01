52a0310c5b31d1e0426963cdf2d814492cba1cd7
Weien Wang <weienw@gmail.com>
2016-09-01 16:14:48 -0500

    [NEW] set up project

---
+++ GroceryCashiers/GroceryCashiers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GroceryCashiers
+//
+//  Created by Weien Wang on 9/1/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
