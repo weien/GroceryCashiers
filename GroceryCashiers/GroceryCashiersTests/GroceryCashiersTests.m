52b7881135af44c5969d41375a4789c2fbc305f4
Weien Wang <weienw@gmail.com>
2016-09-01 22:19:57 -0500

    [ADD] a few more tests

--- GroceryCashiers/GroceryCashiersTests/GroceryCashiersTests.m
+++ GroceryCashiers/GroceryCashiersTests/GroceryCashiersTests.m
@@ -26,9 +26,27 @@
 }
 
 - (void)testExample1 {
-    NSString* example1 = @"1\nA 1 2\nA 2 1";
-    NSDictionary* output = [self.vc runSimulationWithEntry:example1];
+    NSString* example = @"1\nA 1 2\nA 2 1";
+    NSDictionary* output = [self.vc runSimulationWithEntry:example];
     XCTAssertEqual(7,[output[@"minutes"] integerValue]);
+}
+
+- (void)testExample2 {
+    NSString* example = @"2\nA 1 5\nB 2 1\nA 3 5\nB 5 3\nA 8 2";
+    NSDictionary* output = [self.vc runSimulationWithEntry:example];
+    XCTAssertEqual(13,[output[@"minutes"] integerValue]);
+}
+
+- (void)testExample3 {
+    NSString* example = @"2\nA 1 2\nA 1 2\nA 2 1\nA 3 2";
+    NSDictionary* output = [self.vc runSimulationWithEntry:example];
+    XCTAssertEqual(6,[output[@"minutes"] integerValue]);
+}
+
+- (void)testError {
+    NSString* example = @"2\nC 1 2";
+    NSDictionary* output = [self.vc runSimulationWithEntry:example];
+    XCTAssertNotNil(output[@"error"]);
 }
 
 @end
